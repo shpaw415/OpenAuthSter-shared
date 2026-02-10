47453088df182eb0de382224a934a5478a09d026
Justin <shpaw415@gmail.com>
2026-02-10 11:30:40 -0500

    add cookie name invite id

--- index.ts
+++ index.ts
@@ -531,3 +531,4 @@ export function createExternalGlobalProjectConfig<CTXProperties = any>(
 
 export const COOKIE_NAME = "oauth_client_id" as const;
 export const COOKIE_COPY_TEMPLATE_ID = "oauth_copy_template_id" as const;
+export const COOKIE_INVITE_ID = "oauth_invite_id" as const;
