967526e301178c82f1256cd13a8942b4fdc9b102
Justin <shpaw415@gmail.com>
2026-02-19 14:11:30 -0500

    add webhook class helper

--- webhook/index.ts
+++ webhook/index.ts
@@ -1,22 +1,33 @@
-import type { WebHookConfig, WebHookEvents } from "./types";
+import type {
+  WebHookConfig,
+  WebHookEvents,
+  ExtendedWebHookConfig,
+  WebHookPayLoad,
+} from "./types";
 import { drizzle, eq, and } from "../database/drizzle";
 import { WebHookTable } from "../database/schema";
 
 export class WebHook {
   private db: ReturnType<typeof drizzle>;
-  private clientID: string;
 
-  constructor({ db, clientID }: { db: D1Database; clientID: string }) {
+  constructor({ db }: { db: D1Database }) {
     this.db = drizzle(db);
-    this.clientID = clientID;
   }
 
-  async register(event: WebHookEvents, config: WebHookConfig) {
+  async register({
+    event,
+    config,
+    clientID,
+  }: {
+    event: WebHookEvents;
+    config: WebHookConfig;
+    clientID: string;
+  }) {
     this.db
       .insert(WebHookTable)
       .values({
         id: crypto.randomUUID(),
-        clientID: this.clientID,
+        clientID: clientID,
         event,
         url: config.url,
         method: config.method,
@@ -26,52 +37,44 @@ export class WebHook {
       .run();
   }
 
-  async getWebHooks() {
+  async getWebHooks(clientID: string) {
     return this.db
       .select()
       .from(WebHookTable)
-      .where(eq(WebHookTable.clientID, this.clientID))
+      .where(eq(WebHookTable.clientID, clientID))
       .all();
   }
 
   async deleteWebHook(webHookID: string) {
     return this.db
       .delete(WebHookTable)
-      .where(
-        and(
-          eq(WebHookTable.clientID, this.clientID),
-          eq(WebHookTable.id, webHookID),
-        ),
-      )
+      .where(and(eq(WebHookTable.id, webHookID)))
       .run();
   }
   /**
    * Triggers all webhooks for a specific event with the given payload. It handles both POST and GET requests and logs any errors that occur during the process.
    */
-  async trigger(event: WebHookEvents, payload: Record<string, any>) {
+  async trigger(
+    clientID: string,
+    event: WebHookEvents,
+    payload: WebHookPayLoad,
+  ) {
     const webhooks = await this.db
       .select()
       .from(WebHookTable)
       .where(
-        and(
-          eq(WebHookTable.clientID, this.clientID),
-          eq(WebHookTable.event, event),
-        ),
+        and(eq(WebHookTable.clientID, clientID), eq(WebHookTable.event, event)),
       )
       .all();
 
     return Promise.all(
-      webhooks.map(async (webhook) => {
-        const headers =
-          typeof webhook.headers === "string"
-            ? JSON.parse(webhook.headers as string)
-            : webhook.headers || {};
+      webhooks.map(this.parseWebHookConfig).map(async (webhook) => {
         try {
           return await fetch(webhook.url, {
             method: webhook.method,
             headers: {
               "Content-Type": "application/json",
-              ...headers,
+              ...webhook.headers,
             },
             body: JSON.stringify(payload),
           }).then((res) => {
@@ -95,5 +98,51 @@ export class WebHook {
         }
       }),
     );
+  }
+
+  parseWebHookConfig(
+    raw: typeof WebHookTable.$inferSelect,
+  ): ExtendedWebHookConfig {
+    return {
+      id: raw.id,
+      clientID: raw.clientID,
+      url: raw.url,
+      method: raw.method as WebHookConfig["method"],
+      created_at: raw.created_at,
+      event: raw.event as WebHookEvents,
+      headers: raw.headers ? JSON.parse(raw.headers as string) : undefined,
+    };
+  }
+  stringifyWebHookConfig(
+    config: WebHookConfig,
+  ): Partial<typeof WebHookTable.$inferInsert> {
+    return {
+      ...config,
+      headers: config.headers ? JSON.stringify(config.headers) : undefined,
+    };
+  }
+  retriveWebHookFromIssuer({
+    clientID,
+    webHookID,
+    issuerURI,
+    secret,
+  }: {
+    clientID: string;
+    webHookID: string;
+    issuerURI: string;
+    secret: string;
+  }) {
+    return fetch(`${issuerURI}/webhook/${clientID}/${webHookID}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-secret": secret,
+      },
+    }).then((res) => {
+      if (!res.ok) {
+        throw new Error(`Failed to retrieve webhook: ${res.statusText}`);
+      }
+      return res.json() as unknown as WebHookPayLoad;
+    });
   }
 }
--- webhook/types.ts
+++ webhook/types.ts
@@ -8,4 +8,18 @@ export type WebHookConfig = {
   url: string;
   method: "POST" | "GET";
   headers?: Record<string, string>;
+  event: WebHookEvents;
+};
+
+export type ExtendedWebHookConfig = WebHookConfig & {
+  id: string;
+  clientID: string;
+  created_at: string;
+};
+
+export type WebHookPayLoad = {
+  event: WebHookEvents;
+  clientID: string;
+  timestamp: string;
+  id: string;
 };
